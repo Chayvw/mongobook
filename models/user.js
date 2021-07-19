05ae05ab64f995cea1f03643abff623e274468ad
Chay Williams <Chayvw@gmail.com>
2021-07-19 16:41:29 -0400

    added bcrypt npm package and hash the password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 
 const Schema = mongoose.Schema;
@@ -31,7 +32,7 @@ const userSchema = new Schema({
     passwordConfirm: {
         type: String,
         trim: true,
-        required:true,
+        required:false,
         // only will work on .create and .save
         validator: function(el){
             return el ===this.password
@@ -39,7 +40,56 @@ const userSchema = new Schema({
         message: "Passwords are not the same"
     },
 
+//     userSchema.pre("save", async function (next) {
+//         // only will run if password was modified
+//         if(!this.isModified("password"))
+//         return next();
+// // asyncschrous version off has and it will return a promise so we will use await 
+//         this.password = await  bcrypt.hash(this.password, 12)
+
+//         // will set the passwordConfirm will be set to undefined 
+//         this.passwordConfirm = undefined;
+//         next();
+
+
+//     })
+    // userSchema.pre("save", function (next) {
+    //     const user = this
+      
+    //     if (this.isModified("password") || this.isNew) {
+    //       bcrypt.genSalt(10, function (saltError, salt) {
+    //         if (saltError) {
+    //           return next(saltError)
+    //         } else {
+    //           bcrypt.hash(user.password, salt, function(hashError, hash) {
+    //             if (hashError) {
+    //               return next(hashError)
+    //             }
+      
+    //             user.password = hash
+    //             next()
+    //           })
+    //         }
+    //       })
+    //     } else {
+    //       return next()
+    //     }
+    //   })
+
     
+});
+userSchema.pre("save", async function (next) {
+    // only will run if password was modified
+    if(!this.isModified("password"))
+    return next();
+// asyncschrous version off has and it will return a promise so we will use await 
+    this.password = await  bcrypt.hash(this.password, 12)
+
+    // will set the passwordConfirm will be set to undefined 
+    this.passwordConfirm = undefined;
+    next();
+
+
 });
 
 
